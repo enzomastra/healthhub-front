838e888cb9dadaadca9495ae5264d688abb4bc83
enzomastra <mastraenzo@gmail.com>
2024-10-09 15:13:29 -0300

    #8 tab bar

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import Home from '../screens/home';
-import Profile from '../screens/profile';
-import Routines from '../screens/routines';
+import BottomTabNavigator from './BottomTabNavigator';
 
 const Stack = createStackNavigator();
 
 export default function AppNavigator() {
   return (
     <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="Routines" component={Routines} />
+      <Stack.Screen name="Home" component={BottomTabNavigator} />
     </Stack.Navigator>
   );
 }
---
+++ navigation/BottomTabNavigator.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/home';
+import Profile from '../screens/profile';
+import Routines from '../screens/routines';
+import { Image } from 'react-native';
+import icons from '../constants/icons';
+
+const Tab = createBottomTabNavigator();
+
+export default function BottomTabNavigator() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? icons.home : icons.home;
+          } else if (route.name === 'Profile') {
+            iconName = focused ? icons.profile : icons.profile;
+          } else if (route.name === 'Routines') {
+            iconName = focused ? icons.routines : icons.routines;
+          }
+
+          return <Image source={iconName} style={{ width: 24, height: 24 }} />;
+        },
+        tabBarActiveTintColor: '#F05219',
+        tabBarInactiveTintColor: 'gray',
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: '#f8f8f8', 
+          borderTopWidth: 3, 
+          elevation: 0,
+          shadowOpacity: 0, 
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Routines" component={Routines} />
+    </Tab.Navigator>
+  );
+}
