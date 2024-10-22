6842d1b3122098106548d46530a0871ffc8fbe78
enzomastra <mastraenzo@gmail.com>
2024-10-22 15:58:24 -0300

    fix

--- screens/createWorkout.js
+++ screens/createWorkout.js
@@ -98,6 +98,7 @@ const styles = StyleSheet.create({
   input: {
     borderWidth: 1,
     borderColor: '#ccc',
+    backgroundColor: '#fff',
     borderRadius: 8,
     padding: 10,
     marginBottom: 16,
